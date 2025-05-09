e6a231a7751f7694176d9044afcfe3a2a8af29f3
Moaaz Atik <moaaz.atik@gmail.com>
2025-05-09 20:55:21 +0300

    Feature: Cloud Firestore Database for Guest book messages

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -4,10 +4,12 @@
 
 import 'package:firebase_auth/firebase_auth.dart'
     hide EmailAuthProvider, PhoneAuthProvider;
+import 'package:firebasemeetup/guest_book.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 import 'app_state.dart';
+import 'guest_book.dart';
 import 'src/authentication.dart';
 import 'src/widgets.dart';
 
@@ -42,6 +44,21 @@ class HomePage extends StatelessWidget {
           const Header("What we'll be doing"),
           const Paragraph(
             'Join us for a day full of Firebase Workshops and Pizza!',
+          ),
+
+          Consumer<ApplicationState>(
+            builder: (context, appState, _) => Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                if (appState.loggedIn) ...[
+                  const Header('Discussion'),
+                  GuestBook(
+                    addMessage: (message) =>
+                        appState.addMessageToGuestBook(message),
+                  ),
+                ],
+              ],
+            ),
           ),
         ],
       ),

67c62e8f57bb82710d1ceb05f354e8deb4d808bd
Moaaz Atik <moaaz.atik@gmail.com>
2025-05-09 20:05:35 +0300

    Used Material 3

--- lib/main.dart
+++ lib/main.dart
@@ -111,9 +111,9 @@ class App extends StatelessWidget {
         buttonTheme: Theme.of(
           context,
         ).buttonTheme.copyWith(highlightColor: Colors.deepPurple),
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         textTheme: GoogleFonts.robotoTextTheme(Theme.of(context).textTheme),
         visualDensity: VisualDensity.adaptivePlatformDensity,
+        useMaterial3: true,
       ),
       routerConfig: _router,
     );
